6bb39dad4ae756dd19d08171762dfa3f7dbd9403
Claus Lensbøl <cmol@cmol.dk>
2011-06-01 09:07:11 +0200

    Now the form does not load again in the prototype

--- cinemaXXX/movie_prototype.aspx.cs
+++ cinemaXXX/movie_prototype.aspx.cs
@@ -15,56 +15,67 @@ namespace cinemaXXX
 			if (Page.IsPostBack == false) {
 
 			}
-			//Movie stuff = new Movie();
-			//stuff.getSchema();
-			//stuff.id = 2;
-			//stuff.getSchema();
-			//stuff.get();
-			//stuff._dbData["moid"] = "abc";
-			/*var movies = stuff.getAll ();
-			movies[2].createPlaceHolderTable(placeholder1);
-			movies[1].createPlaceHolderTable(placeholder1);
-			*/
-			//stuff.createPlaceHolderTable(placeholder1);
+				
+				//Movie stuff = new Movie();
+				//stuff.getSchema();
+				//stuff.id = 2;
+				//stuff.getSchema();
+				//stuff.get();
+				//stuff._dbData["moid"] = "abc";
+				/*var movies = stuff.getAll ();
+				movies[2].createPlaceHolderTable(placeholder1);
+				movies[1].createPlaceHolderTable(placeholder1);
+				*/
+				//stuff.createPlaceHolderTable(placeholder1);
+				
+				/*People dims = new People();
+				dims.id = 1;
+				dims.get();
+				dims.createPlaceHolderTable(placeholder1);*/
+				
+				//stuff.createPlaceHolderTable(placeholder1);
+				
+	
+				//stuff._dbData["title"] = "en ny titel";
+				//stuff.update();
+				
+				
+				
+				
+				Movie filmA = new Movie();
+				filmA.id = 1;
+				filmA.get();
+				
+				//Movie filmB = new Movie();
+				//filmB.id = 2;
+				//filmB.get();
+				
+				filmA.createPlaceHolderTable(placeholder1);
+				//filmB.createPlaceHolderTable(placeholder1);
+				
+				
+				
+				People personA = new People();
+				personA.id = 1;
+				personA.get();
+				/*
+				
+				
+				filmA.createPlaceHolderTable(placeholder1);
+				//personA.createPlaceHolderTable(placeholder1);
+				filmB.createPlaceHolderTable(placeholder1);*/
 			
-			/*People dims = new People();
-			dims.id = 1;
-			dims.get();
-			dims.createPlaceHolderTable(placeholder1);*/
 			
-			//stuff.createPlaceHolderTable(placeholder1);
-			
-
-			//stuff._dbData["title"] = "en ny titel";
-			//stuff.update();
-			
-			
-			
-			
-			Movie filmA = new Movie();
-			filmA.id = 1;
-			filmA.get();
-			
-			People personA = new People();
-			personA.id = 1;
-			personA.get();
-			
-			Movie filmB = new Movie();
-			filmB.id = 2;
-			filmB.get();
-			
-			filmA.createPlaceHolderTable(placeholder1);
-			//personA.createPlaceHolderTable(placeholder1);
-			filmB.createPlaceHolderTable(placeholder1);
 		}
 		
 		protected void clickmig(object sender, System.EventArgs e)
         {
 			Movie dims = new Movie();
 			dims.fillObjectFromPlaceHolder(placeholder1);
+			placeholder1.Controls.Clear();
 			dims.createPlaceHolderTable(placeholder1);
 			//dims.(placeholder1);
-			dims.insert();
+			dims.update();
         }
 		
 		
