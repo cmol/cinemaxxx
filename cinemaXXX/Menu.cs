6013a721927f881b1ae54b5a1210491928b20153
Claus Lensbøl <cmol@cmol.dk>
2011-06-07 12:25:09 +0200

    Added menu class to draw menu

---
+++ cinemaXXX/Menu.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cinemaXXX
+{
+	public static class Menu
+	{
+		public static void drawMenu(System.Web.UI.HtmlControls.HtmlGenericControl menu)
+		{
+			// Begin menu declaration for all
+			Dictionary<string, string> menuItems = new Dictionary<string, string>();
+			menuItems.Add("Show Movies", "show_movies.aspx");
+			
+			// Begin user menu declaration
+			if (LoginHelper.loggedIn()) {
+				menuItems.Add("Show Users", "show_users.aspx");
+			}
+			
+			// Begin admin menu declaration
+			if (LoginHelper.isAdmin()) {
+				menuItems.Add("Show Admins", "show_admin.aspx");
+			}
+						
+			// Draw menu
+			foreach (var key in menuItems.Keys) {
+				menu.Controls.Add(new LiteralControl("<li onclick=\"location.href='"+menuItems[key]+"'\">"+key+"</li>"));
+			}
+		}
+	}
+}
+
