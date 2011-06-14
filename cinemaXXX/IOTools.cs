58c09e2bca4c4f742217bc194bedce49926758f2
Claus Lensbøl <cmol@cmol.dk>
2011-06-14 10:19:00 +0200

    Cleaned up

--- cinemaXXX/IOTools.cs
+++ cinemaXXX/IOTools.cs
@@ -23,21 +23,16 @@ namespace cinemaXXX
 		{
 			Regex r = new Regex("^[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]");
 			
-			//throw new Exception(date_str.Replace(':', '-').Replace(' ', '-'));
-			
 			// If the string date format is without hours, minutes, seconds
 			if (date_str.IndexOf(" ") == -1) {
 				string[] date_arr = new string[3];
 				date_arr = date_str.Split('-');
-				//throw new Exception(String.Join("--", date_arr));
 				return (r.Match(date_str).Success) ? new DateTime(int.Parse(date_arr[2]), int.Parse(date_arr[1]), int.Parse(date_arr[0]), 0, 0, 0) : new DateTime(int.Parse(date_arr[0]), int.Parse(date_arr[1]), int.Parse(date_arr[2]), 0, 0, 0);
 			}
 			// If the string date format is with hours, minutes, seconds
 			else {
 				string[] date_arr = new string[6];
 				date_arr = date_str.Replace(':', '-').Replace(' ', '-').Split('-');
-				//throw new Exception(int.Parse(date_arr[2]).ToString());
-				//return new DateTime(2011, 3, 3, 20, 59, 59);
 				return (r.Match(date_str).Success) ? new DateTime(int.Parse(date_arr[2]), int.Parse(date_arr[1]), int.Parse(date_arr[0]), int.Parse(date_arr[3]), int.Parse(date_arr[4]), int.Parse(date_arr[5])) : new DateTime(int.Parse(date_arr[0]), int.Parse(date_arr[1]), int.Parse(date_arr[2]), int.Parse(date_arr[3]), int.Parse(date_arr[4]), int.Parse(date_arr[5]));
 			}
 		}
@@ -47,7 +42,6 @@ namespace cinemaXXX
 		public static string cleanString(string str)
 		{
 			return str.Replace("'", "\'").Replace("\"", "\\\"");
-			new DateTime(2011, 3, 3, 20, 59, 59);
 		}
 	}
 }
