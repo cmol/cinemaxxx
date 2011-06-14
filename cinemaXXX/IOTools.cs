ce57bce7e8a354886801063f28cf585f209c2151
Claus Lensbøl <cmol@cmol.dk>
2011-06-14 10:17:52 +0200

    Fixed bug in reqex

--- cinemaXXX/IOTools.cs
+++ cinemaXXX/IOTools.cs
@@ -21,19 +21,23 @@ namespace cinemaXXX
 		// Takes strings in the form of "YYYY/MM/DD", "YYYY/MM/DD HH:MM:SS" or "DD/MM/YYYY", "DD/MM/YYYY HH:MM:SS" and returns a DateTime
 		public static DateTime stringToDateTime(string date_str)
 		{
-			Regex r = new Regex("^[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]");
+			Regex r = new Regex("^[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]");
+			
+			//throw new Exception(date_str.Replace(':', '-').Replace(' ', '-'));
 			
 			// If the string date format is without hours, minutes, seconds
 			if (date_str.IndexOf(" ") == -1) {
 				string[] date_arr = new string[3];
 				date_arr = date_str.Split('-');
-				//int[] ints = date_arr.Select(x => int.Parse(x)).ToArray();
+				//throw new Exception(String.Join("--", date_arr));
 				return (r.Match(date_str).Success) ? new DateTime(int.Parse(date_arr[2]), int.Parse(date_arr[1]), int.Parse(date_arr[0]), 0, 0, 0) : new DateTime(int.Parse(date_arr[0]), int.Parse(date_arr[1]), int.Parse(date_arr[2]), 0, 0, 0);
 			}
 			// If the string date format is with hours, minutes, seconds
 			else {
 				string[] date_arr = new string[6];
 				date_arr = date_str.Replace(':', '-').Replace(' ', '-').Split('-');
+				//throw new Exception(int.Parse(date_arr[2]).ToString());
+				//return new DateTime(2011, 3, 3, 20, 59, 59);
 				return (r.Match(date_str).Success) ? new DateTime(int.Parse(date_arr[2]), int.Parse(date_arr[1]), int.Parse(date_arr[0]), int.Parse(date_arr[3]), int.Parse(date_arr[4]), int.Parse(date_arr[5])) : new DateTime(int.Parse(date_arr[0]), int.Parse(date_arr[1]), int.Parse(date_arr[2]), int.Parse(date_arr[3]), int.Parse(date_arr[4]), int.Parse(date_arr[5]));
 			}
 		}
@@ -43,6 +47,7 @@ namespace cinemaXXX
 		public static string cleanString(string str)
 		{
 			return str.Replace("'", "\'").Replace("\"", "\\\"");
+			new DateTime(2011, 3, 3, 20, 59, 59);
 		}
 	}
 }
